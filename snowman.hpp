c9edb7c6281c1707bc3ab336095bfbd9325af921
itamaralmog <itamaralmoghermel@gmail.com>
2021-03-09 17:50:15 +0200

    finish

---
+++ snowman.hpp
@@ -0,0 +1,9 @@
+ #ifndef SNOWMAN
+ #define SNOWMAN
+#include <string>
+namespace ariel{
+    std::string snowman(int forsnow);
+    std::string snowmann(int forsnow);
+    std::string snowmanp(int arrsnow[]);
+};
+ #endif 
